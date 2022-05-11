91350332656394755110acd1d8444a0d3645376b
namomi <xicodey@gmail.com>
2022-05-11 12:07:59 +0900

    스프링 시큐리티를 이용한 회원 가입 및 로그인 기능 만듬

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,11 +1,10 @@
-package com.shop.controoler;
+package com.shop.controller;
 
 import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
